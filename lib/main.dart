dcfe5bbf81a02c0908f9e8bbb110a270f29a6573
sudipeb <sudip.koirala@ebpearls.com>
2026-03-16 12:22:19 +0545

    :up: update dotenv and connecty cube service

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:connectchat/core/services/connectycube_services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env_dev");
+  await ConnectyCubeService.instance.init(dotenv.env['APP_ID']!, dotenv.env['AUTH_KEY']!, dotenv.env['AUTH_SECRET']!);
   runApp(MyApp());
 }
 
